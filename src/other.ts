9ec80485c3f5e234bc7ad2aa2866efe4521ed2b2
gbaranski <gbaranski19@gmail.com>
2020-06-24 05:59:06 +0200

    add devices and localIpAdress

---
+++ src/other.ts
@@ -0,0 +1,8 @@
+export enum devices {
+  Alarmclock = "Alarmclock",
+  Watermixer = "Watermixer",
+}
+export enum localIpAddress {
+  Alarmclock = "192.168.1.110",
+  Watermixer = "192.168.1.120",
+}
