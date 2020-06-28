6423b0c2012b3efb21b5d215dd5df5ec3bfc70ce
gbaranski <gbaranski19@gmail.com>
2020-06-28 13:37:35 +0200

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { AlarmclockData, AlarmclockHeaders } from "./alarmclock";
 import { WatermixerData } from "./watermixer";
-import { Devices, LocalIpAddress, TempArray } from "./other";
+import {
+  Devices,
+  LocalIpAddress,
+  AlarmRequestType,
+  WaterRequestType,
+  TempArray,
+  RequestHistory,
+  OtherRequestsType,
+} from "./other";
 
 export {
   AlarmclockData,
   AlarmclockHeaders,
   WatermixerData,
   Devices,
+  AlarmRequestType,
+  WaterRequestType,
   LocalIpAddress,
   TempArray,
+  RequestHistory,
+  OtherRequestsType,
 };
